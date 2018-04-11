751d6b473016bfb94cbd4d9b1e89fe48544495bc
Wei2015 <wei.yao@sjsu.edu>
2018-04-10 23:59:06 -0700

    add toolbar and navigation in customer_activity

--- app/src/main/java/com/cmpe277/android/takeoutorderms/MainCustomerActivity.java
+++ app/src/main/java/com/cmpe277/android/takeoutorderms/MainCustomerActivity.java
@@ -1,13 +1,101 @@
 package com.cmpe277.android.takeoutorderms;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class MainCustomerActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_customer);
+
+        //set up navi bar
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
+
+        //set up toolbar
+        toolbar = findViewById(R.id.toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    //set up navigation drawer
+    private void setupDrawerContent(final NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+
+                        switch (menuItem.getItemId()){
+                            case R.id.account:
+
+                                break;
+
+                            case R.id.order:
+
+                                break;
+
+                        }
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cart:
+
+                break;
+
+            case R.id.sign_out:
+
+
+                break;
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
     }
 }
